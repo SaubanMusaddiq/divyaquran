b29138053ba2619985e0c172753afa251058fbd2
Sauban Musaddiq <saubanm@amazon.com>
2023-08-27 20:14:31 -0700

    Using Express js

---
+++ app.js
@@ -0,0 +1,17 @@
+const http = require('http'),
+  fs = require('fs'),
+  express = require('express');
+
+const app = express()
+const port = 3000
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req,res) {
+  res.sendfile('public/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
